74a03079441f9d6484bc7af8afd76e88a003ff31
Mitesh Ashar <email@miteshashar.com>
2013-06-07 17:04:40 +0530

    Realised cannot use relative imports here, set them back to absolute.

--- manage.py
+++ manage.py
@@ -5,9 +5,9 @@ from flask.ext.script.commands import Clean, ShowUrls
 from flask.ext.alembic import ManageMigrations, FlaskAlembicConfig
 from alembic import command
 
-from . import app, init_for
-from . import models
-from .models import db
+from hgapp import app, init_for
+from hgapp import models
+from hgapp.models import db
 
 
 manager = Manager(app)
