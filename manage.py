a44c309ea731b05b874b104f81aacc106bd4d2a8
Kiran Jonnalagadda <kiran@hasgeek.com>
2015-11-05 12:59:31 +0530

    Pass in additional context for shell.

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 
 from coaster.manage import init_manager
 
+import hgapp
+import hgapp.models as models
+import hgapp.forms as forms
+import hgapp.views as views
 from hgapp.models import db
 from hgapp import app, init_for
 
 
 if __name__ == '__main__':
     db.init_app(app)
-    manager = init_manager(app, db, init_for)
+    manager = init_manager(app, db, init_for, hgapp=hgapp, models=models, forms=forms, views=views)
     manager.run()
